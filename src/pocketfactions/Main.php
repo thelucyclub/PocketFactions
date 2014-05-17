bfc2c1c38f27a5ea6daa7b228084db79e1c98999
ijoshuahd <joshuamanuta@me.com>
2014-05-17 14:29:26 +0800

    added commands

--- src/pocketfactions/Main.php
+++ src/pocketfactions/Main.php
@@ -57,17 +57,149 @@ class Main extends Prt implements Listener, EvtExe{
 		$this->getServer()->getPluginManager()->registerEvent("pocketmine\\event\\$simpleName", $this, $priority, $this, $this);
 	}
 	protected function registerCmds(){
+	//Faction Commands for Players
 		$main = new PCmd("faction", $this);
 		$main->setUsage("/faction <help|cmd>");
 		$main->setDescription("PocketFactions commands");
 		$main->setPermission("pocketfactions.cmd");
 		$main->setAliases(array("f"));
-		$main->reg();
+	//Faction Commands for Server Owners
+		$main2 = new PCmd("fmanager", $this);
+		$main2->setUsage("/fmanager <wclaim|smoney|gmoney|fdelete>");
+		$main2->setDescription("PocketFactions Admin commands");
+		$main2->setPermission("admin.pocketfactions.cmd");
+		$main2->setAliases(array("frmgr"));
+		$main2->reg();
 	}
 	public function getDb(){
 		return $this->db;
 	}
 	public function onCommand(Issuer $issuer, Command $cmd, $lbl, array $args){
+	switch($cmd->getName()){
+	
+		case "fmgr":
+		case "faction":
+				if(!($issuer instanceof Player)){
+				$issuer->sendMessage("-=[ Pocket Faction Commands (P.1/2) ]=-");
+				$issuer->sendMessage("/f create - Create a Faction.");
+				$issuer->sendMessage("/f invite - Invite someone in your Faction.");
+				$issuer->sendMessage("/f accept - Accept Faction Invitation.");
+				$issuer->sendMessage("/f decline - Decline Faction Invitation.");
+				$issuer->sendMessage("/f join - Join public Faction.");
+				$issuer->sendMessage("/f claim - Claim areas for your Faction.");
+				$issuer->sendMessage("/f unclaim - Unclaim areas by your Faction.");
+				$issuer->sendMessage("/f unclaimall - Unclaim all areas by your Faction.");
+				$issuer->sendMessage("/f kick - Kick someone in your Faction.");
+				$issuer->sendMessage("/f setperm - Set permissions in your Faction.");
+				$issuer->sendMessage("/f sethome - Set Faction home.");
+				$issuer->sendMessage("/f home - Teleport back to Faction home.");
+				$issuer->sendMessage("/f money - View Faction Money balance.");
+				$issuer->sendMessage("/f quit - Quit a Faction.");
+				$issuer->sendMessage("/f disband - Disband your Faction.");
+				if($issuer instanceof Console)
+					$isuer->sendMessage("Run this command in-game.");
+				return true;
+			}
+			$subcmd = @array_shift($args);
+			switch($subcmd){ // manage subcommand
+				case "help":
+				if(isset($args[0])){
+						if(strtolower($args[0]) === "1"){
+							$issuer->sendMessage("-=[ Pocket Faction Commands (P.1/3) ]=-");
+							$issuer->sendMessage("/f create - Create a Faction.");
+							$issuer->sendMessage("/f invite - Invite someone in your Faction.");
+							$issuer->sendMessage("/f accept - Accept Faction Invitation.");
+							$issuer->sendMessage("/f decline - Decline Faction Invitation.");
+							$issuer->sendMessage("/f join - Join public Faction.");
+							break;
+						}
+						if(strtolower($args[0]) === "2"){
+							$issuer->sendMessage("-=[ Pocket Faction Commands (P.2/3) ]=-");
+							$issuer->sendMessage("/f claim - Claim areas for your Faction.");
+							$issuer->sendMessage("/f unclaim - Unclaim areas by your Faction.");
+							$issuer->sendMessage("/f unclaimall - Unclaim all areas by your Faction.");
+							$issuer->sendMessage("/f kick - Kick someone in your Faction.");
+							$issuer->sendMessage("/f setperm - Set permissions in your Faction.");
+							$issuer->sendMessage("/f sethome - Set Faction home.");
+							break;
+						}
+						if(strtolower($args[0]) === "3"){
+							$issuer->sendMessage("-=[ Pocket Faction Commands (P.3/3) ]=-");
+							$issuer->sendMessage("/f home - Teleport back to Faction home.");
+							$issuer->sendMessage("/f money - View Faction Money balance.");
+							$issuer->sendMessage("/f quit - Quit a Faction.");
+							$issuer->sendMessage("/f disband - Disband your Faction.");
+							break;
+						}
+					}
+					$issuer->sendMessage("-=[ Pocket Faction Commands (P.1/3) ]=-");
+					$issuer->sendMessage("/f create - Create a Faction.");
+					$issuer->sendMessage("/f invite - Invite someone in your Faction.");
+					$issuer->sendMessage("/f accept - Accept Faction Invitation.");
+					$issuer->sendMessage("/f decline - Decline Faction Invitation.");
+					$issuer->sendMessage("/f join - Join public Faction.");
+					break;
+				
+				case "create":
+				//TODO code
+				break;
+				
+				case "invite":
+				//TODO code
+				break;
+				
+				case "accept":
+				//TODO code
+				break;
+				
+				case "decline":
+				//TODO code
+				break;
+				
+				case "join":
+				//TODO code
+				break;
+				
+				case "claim":
+				//TODO code
+				break;
+				
+				case "unclaim":
+				//TODO code
+				break;
+				
+				case "unclaimall"
+				//TODO code
+				break;
+				
+				case "kick":
+				//TODO code
+				break;
+				
+				case "setperm":
+				//TODO code
+				break;
+				
+				case "sethome":
+				//TODO code
+				break;
+				
+				case "home":
+				//TODO code
+				break;
+				
+				case "money":
+				//TODO code
+				break;
+				
+				case "quit":
+				//TODO code
+				break;
+				
+				case "disband":
+				//TODO code
+				break;
+				
 	}
 	public static function get(){
 		return Server::getInstance()->getPluginManager()->getPlugin(self::NAME);
