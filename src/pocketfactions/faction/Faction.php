aece961f477f15f52125e2a9731bc838fdc4bdd8
PEMapModder <pemapmodder1970@gmail.com>
2014-05-25 22:36:22 +0800

    Added getters in Faction.php

--- src/pocketfactions/faction/Faction.php
+++ src/pocketfactions/faction/Faction.php
@@ -4,7 +4,36 @@ namespace pocketfactions\faction;
 
 class Faction{
 	protected $name;
-	protected $players = array();
+	protected $id;
+	protected $founder;
+	protected $ranks;
+	protected $defaultRank;
+	protected $members;
+	protected $chunks;
+	protected $baseChunk;
 	public function __construct(array $args){
+	}
+	public function getName(){
+		return $this->name;
+	}
+	public function getID(){
+		return $this->id;
+	public function getFounder(){
+		return $this->founder;
+	}
+	public function getRanks(){
+		return $this->ranks;
+	}
+	public function getDefaultRank(){
+		return $this->defaultRank;
+	}
+	public function getMembers(){
+		return $this->members:
+	}
+	public function getChunks(){
+		return $this->chunks;
+	}
+	public function getBaseChunk(){
+		return $this->baseChunk;
 	}
 }
