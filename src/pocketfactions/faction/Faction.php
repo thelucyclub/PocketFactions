aaa9956fd99b07866e7b36ecdbf67d92819e319d
DinoKill <newlawrence2498@gmail.com>
2014-05-27 13:30:33 +0100

    Added documentation and fixed syntax error.
    Added documentation and a closing bracket for the getID() function.

--- src/pocketfactions/faction/Faction.php
+++ src/pocketfactions/faction/Faction.php
@@ -21,27 +21,87 @@ class Faction{
 		$this->chunks = $args["chunks"];
 		$this->baseChunk = $args["base-chunk"];
 	}
+	
+       /**
+        * 
+        * Gets the name of a faction.
+        *
+        * @return string The name of the faction.
+        *
+        */
 	public function getName(){
 		return $this->name;
 	}
+	
+       /**
+        * 
+        * Gets the ID of a faction.
+        *
+        * @return int The ID of the faction.
+        *
+        */
 	public function getID(){
 		return $this->id;
+	}
+
+       /**
+        * Gets the Player object for the 
+        * person who started the faction.
+        *
+        * @return Player The founder of this faction.
+        *
+        */	
 	public function getFounder(){
 		return $this->founder;
 	}
+	
+        /**
+         * Gets the name of a faction.
+         *
+         * @return string The name of the faction.
+         *
+         */
 	public function getRanks(){
 		return $this->ranks;
 	}
+	
+       /**
+        * Gets the default rank for this faction.
+        *
+        * @return Rank The default of the faction.
+        *
+        */
 	public function getDefaultRank(){
 		return $this->defaultRank;
 	}
+	
+       /**
+        * Gets an array of all members in this faction.
+        *
+        * @return Player[] An array of faction members.
+        *
+        */
 	public function getMembers(){
-		return $this->members:
+	    return $this->members:
 	}
+      
+       /**
+        * Gets all chunks claimed by this faction.
+        *
+        * @return Chunk[] The default of the faction.
+        *
+        */	
 	public function getChunks(){
 		return $this->chunks;
 	}
+	
+       /**
+        * Get the base chunk for this faction.
+        *
+        * @return Chunk The base chunk.
+        *
+        */	
 	public function getBaseChunk(){
-		return $this->baseChunk;
+            return $this->baseChunk;
 	}
 }
