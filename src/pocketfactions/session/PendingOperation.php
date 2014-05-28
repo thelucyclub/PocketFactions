5bd7690ee27a3a8ac3281db36c7a0600c69b7a3e
PEMapModder <pemapmodder1970@gmail.com>
2014-05-28 19:03:38 +0800

    Update PendingOperation.php

--- src/pocketfactions/session/PendingOperation.php
+++ src/pocketfactions/session/PendingOperation.php
@@ -2,8 +2,10 @@
 
 abstract class PendingOperation{
 	protected $op;
+	public static $poid = 0;
 	public function __construct(callable $onOp){
 		$this->op = $op;
+		$this->id = self::$poid++;
 	}
 	public function onOp(){
 		call_user_func_array($this->op, func_get_args());
