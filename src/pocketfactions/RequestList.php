4fcc36a997042a7b48842ead4609a6f59de37259
PEMapModder <pemapmodder1970@gmail.com>
2014-05-31 11:12:33 +0800

    Partial req  implementation

--- src/pocketfactions/RequestList.php
+++ src/pocketfactions/RequestList.php
@@ -13,4 +13,14 @@ class RequestList implements Listener{
 	public function add(Request $request){
 		$this->list[strtolower($request->getTo()->getName())][$request->getStrId()] = $request;
 	}
+	public function onJoin(PlayerJoinEvent $evt){
+		$name = strtolower($evt->getPlayer()->getName());
+		$data = $this->list[$name];
+		$p = $evt->getPlayer();
+		$p->sendMessage("You have ".count($data)." unprocessed requests.\nUse /req accept <id>, /req decline <id> to accept/decline; use /req read [id] to read the request(s) again.");
+		foreach($data as $k=>$req){
+			$n = 1;
+			$p->sendMessage("Request #".($n++).: ID $k; Content: ".$req->getContent());
+		}
+	}
 }
