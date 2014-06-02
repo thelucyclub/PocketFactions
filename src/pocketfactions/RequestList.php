516cca4dbe282f024e9fa21621e79c3368fda772
PEMapModder <pemapmodder1970@gmail.com>
2014-06-02 17:55:14 +0800

    Added database format documentation

--- src/pocketfactions/RequestList.php
+++ src/pocketfactions/RequestList.php
@@ -7,6 +7,8 @@ use pocketmine\event\player\PlayerCommandPreprocessEvent as PreCmdEvt;
 use pocketmine\event\player\PlayerJoinEvent;
 
 class RequestList implements Listener{
+	/** @var Request[][] */
+	private $list = array();
 	public function __construct(){
 		$this->server = Server::getInstance();
 		$this->main = Main::get();
@@ -20,9 +22,10 @@ class RequestList implements Listener{
 		$data = $this->list[$name];
 		$p = $evt->getPlayer();
 		$p->sendMessage("You have ".count($data)." unprocessed requests.\nUse /req accept <id>, /req decline <id> to accept/decline; use /req read [id] to read the request(s) again."); // unprocessed is not a good verb. Please, English experts (LDX seems to be one), please optimize this sentence.
+		$n = 1;
 		foreach($data as $k=>$req){
-			$n = 1;
-			$p->sendMessage("Request #".($n++).": ID $k; Content: ".$req->getContent());
+			$p->sendMessage("Request #".($n++)."" .
+				": ID $k; Content: ".$req->getContent());
 		}
 	}
 	public function onPreCmd(PreCmdEvt $evt){
